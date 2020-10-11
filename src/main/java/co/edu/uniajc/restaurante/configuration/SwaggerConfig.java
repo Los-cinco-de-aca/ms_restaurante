bbcc54c644152e976a72df6a67791ec371be339b
James Arias <james.arias.ajc@gmail.com>
2020-10-10 19:33:45 -0500

    Swagger

--- src/main/java/co/edu/uniajc/restaurante/configuration/SwaggerConfig.java
+++ src/main/java/co/edu/uniajc/restaurante/configuration/SwaggerConfig.java
@@ -1,17 +1,25 @@
 package co.edu.uniajc.restaurante.configuration;
 
-/*@Configuration
-@EnableSwagger2*/
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
-	/*@Bean
+	@Bean
 	public Docket api(){
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("co.edu.uniajc.restaurante.controller"))
 				.paths(PathSelectors.regex("/.*"))
-				.build();
-			
-	}*/
+				.build();	
+	}
 	
 }
