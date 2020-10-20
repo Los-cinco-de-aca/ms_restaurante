50da86752f3d878c6a8cffeddbc7c3dc37e825db
Jonathan-Palomino-10 <jonathan.palomino.ajc@gmail.com>
2020-10-19 18:33:33 -0700

    Se realizan ajuste en la autenticacion

--- src/main/java/co/edu/uniajc/restaurante/controller/AuthController.java
+++ src/main/java/co/edu/uniajc/restaurante/controller/AuthController.java
@@ -2,15 +2,11 @@ package co.edu.uniajc.restaurante.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.uniajc.restaurante.dto.LoginDTO;
-import co.edu.uniajc.restaurante.entities.Pedido;
 import co.edu.uniajc.restaurante.service.AuthService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
