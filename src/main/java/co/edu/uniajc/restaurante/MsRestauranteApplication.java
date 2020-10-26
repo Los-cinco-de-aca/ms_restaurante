1fdefa81ff55a1c36d57815ef6e33daa18511711
Jonathan-Palomino-10 <jonathan.palomino.ajc@gmail.com>
2020-10-25 20:32:09 -0700

    Ajustes swagger final

--- src/main/java/co/edu/uniajc/restaurante/MsRestauranteApplication.java
+++ src/main/java/co/edu/uniajc/restaurante/MsRestauranteApplication.java
@@ -29,7 +29,11 @@ public class MsRestauranteApplication {
     @Configuration
     class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
- 
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+        }
+
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -65,15 +69,7 @@ public class MsRestauranteApplication {
                 
         }
         
-        @Configuration
-        public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-               @Override
-               public void configure(WebSecurity web) throws Exception {
-                   web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
-               }
-
-        }
     }
 
 }
